b00e8a53fb4fa71ae7665fd480384675c55744a2
Bruno725-1 <bruuno.laraujo@gmail.com>
2024-12-10 15:04:21 +0000

    Um commit qualquer. An any commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program{
+    public static void Main(){
+        int a, b, c;
+        Console.WriteLine("Type 2 numbers");
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = a + b;
+        Console.WriteLine("The sum of {0} and {1} is: {2}",a,b,c);
+        double d + double e = double f;
+    }
+}
